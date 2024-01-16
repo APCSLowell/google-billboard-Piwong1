aec56c2665ef141a2353ae5638f5670d4658aa57
Piwong1 <142533789+Piwong1@users.noreply.github.com>
2024-01-16 11:04:16 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,9 +1,10 @@
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-boolean found=false;
-public void setup()  
-{            
-    //your code here
-   
+
+
+public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public static void main(final String[] args) {
+		System.out.println("Not the right string, tests will fail!");
+         boolean found=false;
     for(int i=2;i<e.length()-10;i++){
      String digits = e.substring(i,i+10);
 double dNum = Double.parseDouble(digits);
@@ -12,20 +13,17 @@ if(isPrime(dNum)==true&&found==false){
    found=true;
       }
     }
-    
-}  
-public void draw()  
-{   
-  //not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
-    //your code here   
-    
-   for(int i=2;i<=Math.sqrt(dNum);i++){
+	}
+	
+
+	//Finish this function
+	public boolean isPrime(){
+        for(int i=2;i<=Math.sqrt(dNum);i++){
     if(dNum%i==0){
       return false;
     }
     }
     return true;  
-} 
+	}
+
+}
